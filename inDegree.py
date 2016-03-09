c478a335ed64db8c9f4b3b6e031d67d0de7d9851
TejasShinde <tjshinde@gmail.com>
2016-03-09 13:30:20 -0800

    Adding explanations

--- inDegree.py
+++ inDegree.py
@@ -6,12 +6,12 @@ import matplotlib.pyplot as plt
 
 def visualize(output):
 	with open(output, newline='') as tsvfile:
-		spamreader = csv.reader(tsvfile, delimiter='\t', quotechar='|')
+		filereader = csv.reader(tsvfile, delimiter='\t', quotechar='|')
 		degreeDist = list()
-		for row in spamreader:
+		for row in filereader:
 			degreeDist.append(int(row[1]))
-	dist = np.array(degreeDist)
-	plt.hist(dist, bins=100, histtype='stepfilled', log=True)
+	distrib = np.array(degreeDist)
+	plt.hist(distrib, bins=100, histtype='stepfilled', log=True)
 	plt.xlabel('In-degree value')
 	plt.ylabel('Node frequency')
 	plt.title('Incoming edge distribution')
